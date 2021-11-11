231fc9fba98dd01ff7c89abfa5cba949bcf0dfc6
Henry_Zhu <38650110+HenryZhuHR@users.noreply.github.com>
2021-11-11 09:19:13 +0800

    NEW

--- src/func_dct.m
+++ src/func_dct.m
@@ -1,12 +1,14 @@
 function out = func_dct(image)
     T = dctmtx(8); % 8*8DCT
     % dctmtx(N)可以生成N*N的DCT变换矩阵。
-    % 对图像进行二维DCT变换有两种方法。第一种：直接使用dct2()函数。第二种，用dctmtx()获取DCT变换矩阵，再T×A×T'。
-    % 反变换时T'×A×T即可
+    % 对图像进行二维DCT变换有两种方法：
+    %   1 直接使用dct2()函数。
+    %   2 用dctmtx()获取DCT变换矩阵，再T×A×T'。反变换T'×A×T
     func=@(block) T*block.data*T'
     for i=1:3
         out(:,:,i) = blockproc(image(:,:,i), [8 8], 'P1*x*P2', T, T');
         % blkproc函数为分块操作函数，即对所有8*8的块执行DCT。
-        % 对于不能整除的情况，会自动用0填充右边缘和下边缘，在计算结束后自动删除它们
+        % 对于不能整除的情况，会自动用0填充右边缘和下边缘，
+        % 在计算结束后自动删除它们
     end
 end
